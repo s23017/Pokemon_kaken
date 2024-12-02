1555c84f8064aa411d346dfbe1d69b60244cc283
itc-s23012 <s23012@std.it-college.ac.jp>
2024-12-02 09:50:49 +0900

    変えた場所の修正

--- src/app/api/pokemon.js
+++ src/app/api/pokemon.js
@@ -26,4 +26,4 @@ export const fetchAdvantageousPokemons = async (type) => {
     const response = await fetch(`https://pokeapi.co/api/v2/type/${type}`);
     const data = await response.json();
     return data.pokemon.map(pokemonEntry => pokemonEntry.pokemon.name);
-}
+};
